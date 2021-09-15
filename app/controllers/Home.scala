69792366fc579e1b1391d7fab2ce4bc4000e6ad3
Kanta Inoue <kanta.inoue@nextbeat.net>
2021-09-15 15:32:12 +0900

    更新機能実装

--- app/controllers/Home.scala
+++ app/controllers/Home.scala
@@ -6,14 +6,12 @@
 
 package controllers
 
-import ixias.model.Entity.WithNoId
 import lib.model.User.Status.IS_INACTIVE
 import lib.persistence.onMySQL._
 
 import scala.concurrent.duration.Duration
 import play.api.i18n.I18nSupport
 import play.api.data.Form
-import play.api.data.Forms._
 
 import javax.inject._
 import play.api.mvc._
@@ -21,8 +19,6 @@ import model._
 import lib.model.User
 import lib.model.UserCategory
 import model.TodoData._
-import views.html.helper.form
-import play.api.mvc.Result
 
 import scala.concurrent.Await
 
@@ -85,6 +81,57 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents)
         Redirect("/userList")
       }
     )
-
   }
+
+  def edit(Id:  Int) = Action { implicit request =>
+    val vv = ViewValueStore(
+      title   = "TODO編集",
+      cssSrc  = Seq("store.css"),
+      jsSrc   = Seq("userList.js")
+    )
+    val allUser    = UserRepository.getAll()
+    val userList   = Await.result(allUser, Duration.Inf)
+    val user       = userList.find(v => v.id.get.toInt == Id)
+    user match {
+      case None   =>  Ok("/userList")
+      case Some(info)  =>
+        val category  = {
+        val categoryId  = info.category_id
+        categoryId match  {
+          case  1 =>  "フロントエンド"
+          case  2 =>  "バックエンド"
+          case  3 =>  "インフラ"
+        }
+      }
+        val getUser  = user.get
+        Ok(views.html.Edit(vv, getUser.id.get, todoForm.fill(TodoData(getUser.title,  getUser.body,  category))))
+    }
+  }
+
+  def update(Id:  Int) = Action { implicit request =>
+    val vv = ViewValueStore(
+      title = "TODO登録",
+      cssSrc = Seq("edit.css"),
+      jsSrc = Seq("userList.js")
+    )
+    val allUser    = UserRepository.getAll()
+    val userList   = Await.result(allUser, Duration.Inf)
+    val user       = userList.find(v => v.id.get.toInt == Id)
+    todoForm.bindFromRequest().fold(
+      (formWithErrors: Form[TodoData]) => {
+        BadRequest(views.html.Edit(vv, user.get.id.get, formWithErrors))
+      },
+      (todoFormData: TodoData) => {
+        val cId         = if (todoFormData.categoryName == "フロントエンド") 1 else if (todoFormData.categoryName == "バックエンド") 2 else 3
+        val userInfo    = UserRepository.get(user.get.id.get)
+        val userRecord  = Await.result(userInfo, Duration.Inf).get
+        val EditUser    = userRecord.map(_.copy(title = todoFormData.title, body = todoFormData.body, category_id = cId))
+        UserRepository.update(EditUser)
+
+        Redirect("/userList")
+      }
+    )
+  }
+
+
 }
